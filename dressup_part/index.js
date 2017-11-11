72a65bb1415b554edbd49850dae2f731a957cb0f
Admin <artem.a@cronix.ms>
2017-11-11 20:28:55 +0300

    add fix for ui
    add fix for ui

--- dressup_part/index.js
+++ dressup_part/index.js
@@ -3,8 +3,18 @@ angular.module("dress", [])
         $scope.clothes_layer=0
         $scope.clothes_type=0
         $scope.character = {
-            body :'../img/body/02.png'
+            body :1
         }
+        $scope.body_color=[
+            '../img/body/01.png',
+            '../img/body/02.png',
+            '../img/body/03.png',
+            '../img/body/04.png',
+            '../img/body/05.png',
+            '../img/body/06.png',
+            '../img/body/07.png',
+            '../img/body/08.png'
+        ]
         $scope.countArt = function () {
 
             $timeout($scope.countArt, 1000);
